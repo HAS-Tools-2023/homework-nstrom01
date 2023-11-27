3aa27f76c007fe624aff577429350e6aa01d219b
nstrom01 <nstrom@arizona.edu>
2023-11-27 12:00:17 -0700

    Week 13 HW Submission

---
+++ Homework_Submissions/Weekly_Assignments/strom_hw13.md
@@ -0,0 +1,20 @@
+
+# WEEK 13 Reflection  
+
+## Changes Made on Submitted Code
+I made the following changes on this weeks code from the last version
+- Style & Readability: I adhered to PEP8 style guide on this version. I used a linter and ensured specific things such as character line length, spacing, and indentation were followed. This helped readability, however some of the linter suggestions seemed a bit unnecessary as in they did not seem to improve readability, but I followed them for good practice. 
+- Use of Functions: I used 4 functions in this code, 1 main function with 3 embedded functions. This was done for efficiency and reduction of code. This was a useful approach, however there are restrictions as it is more difficult to write a function within the function and test-run it's capabilities. 
+- Flow: I improved the flow of this code from the last. I tried to define variables that would be repeatively used, and reduce the amount of variables created. I have a habit of creating many variables and this can get confusing and excessive. There seems to be an art to knowing when it is best to create a variable or embed a procedure into a code. Sometimes adding variables can help for readability and comprehension, but too many can reduce readablity. 
+  
+
+## Areas for Improvement on code
+- Defining function variables: I realized toward the end of my code that you do not need to define inputs for embedded functions (functiosn w/ in functions). Rather you can just use variables defined prior to the function, granted they were defined within the main funciton. Therefore, I could have reduced code by doing this, and it is much more concise using prior defined variables, than taking a variable and solely renaming it. 
+- Conciseness: Despite my flow of code improving, I think I could improve the conciseness. Perhaps I could use loops instead of functions, however I am better with writing functions. My outline and structure of code could have been writen more concise, but it seems that many of these realizations in efficiency are seen towards the end of the code when I am finishing, and changing certain approaches could mean scraping much of the code. I am sure I will get better at this with more practice and experience. 
+
+
+## chatGPT
+- I did not use chatGPT very much for this code. I used it here and there to solve some smaller issues of a few lines, and did this before seeing the instructions that said to post a pre chatGPT draft. I used it mostly for issues when I could not figure out why my linter was mad at me, and it usually came down to a very subtle spacing or indentation issue.
+- I think chatGPT can be helpful for specific problems, and smaller lines of codes, but not when it comes to larger structuring, or more complex interconnected code areas. For curosity, I copy pasted my final code into chatGPT to see if it could be improved, and while the organization improved very much, it would not run properly, so I kept my mostly chatGPTless code for submission. 
+
+#### Nathan Strom - 11/27/2023
---
+++ Homework_Submissions/Weekly_Assignments/strom_hw13_DRAFT.py
@@ -0,0 +1,220 @@
+
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+#%%
+
+"""
+Enter the Monday date of the forecast week of interest.
+A prediction of the average flow for the entered week and the following week will print.
+"""
+forecast_date_week1 = '2023-11-27'
+
+
+def find_streamflow_prediction_from_usgs_stream(site_number='09506000',
+                                                data_start_date='1989-01-01',
+                                                data_end_date='2023-11-25',
+                                                wk1_predict_date='2023-11-27',
+                                                trim_range=.5, std_size=1):
+    """
+    Predicts streamflow based on historical data.
+
+    Inputs:
+    - site_number (str): USGS site number.
+    - data_start_date (str): Start date for historical data.
+    - data_end_date (str): End date for historical data.
+    - wk1_predict_date (str): Date for the one-week prediction.
+    - trim_range (float): Range for trimming similar flow years.
+    - std_size (int): Multiplier for standard deviation.
+
+    Outputs: tuple: A tuple containing two elements:
+    - float: One week out flow prediction.
+    - float: Two week out flow prediction.
+    - plot: Historical flow averages w/ forecast averages
+
+
+    Inner Functions:
+    - 'find_yearly_flow_mean': Finds the yearly mean flow for a given date range.
+    - 'find_flow_forecast_by_week': Finds the flow forecast for a given week.
+    - 'plot_week_flow_avg_by_year': Plots weekly avgs and forecast avgs
+
+    Description:
+        Predicts streamflow based on historical data for a specified site and date range. 
+        Calcs dates, computes datasets, finds similar flow years, 
+        and predicts flow for 1 and 2 wks out.
+
+
+    Note: All inner functions are used to support the main prediction functionality.
+        A smaller 'trim_range' & 'std_size' does not always yield a smaller forecast. 
+    """
+
+    # Reading in file from URL and setting data
+    url = (
+        "https://waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no=" 
+        + site_number
+        + "&referred_module=sw&period=&begin_date=" 
+        + data_start_date + "&end_date=" 
+        + data_end_date)
+    data = pd.read_table(
+        url, skiprows=30,
+        names=['agency_cd', 'site_no', 'datetime', 'flow', 'code'],
+        parse_dates=['datetime'],
+        index_col=['datetime'])
+
+    # Setting the dates needed for dataframes from the input wk1 date
+    wk1_predict_date = pd.to_datetime(wk1_predict_date)
+    pst_2wk_strt_d8 = wk1_predict_date - pd.DateOffset(days=15)
+    last_day_d8 = wk1_predict_date - pd.DateOffset(days=1)
+    wk2_predict_d8 = wk1_predict_date + pd.DateOffset(days=7)
+    crnt_yr = last_day_d8.year
+    wk1_predict_yr = wk1_predict_date.year
+    wk2_predict_yr = wk2_predict_d8.year
+
+    # Making datasets that will be used for calc later in code
+    def find_yearly_flow_mean(df=data, start_date=pst_2wk_strt_d8,
+                              end_date=last_day_d8):
+        """
+        Finds the yearly mean flow for a given date range.
+
+        Inputs:
+        - df (pd.DataFrame): Input DataFrame.
+        - start_date (pd.Timestamp): Start date.
+        - end_date (pd.Timestamp): End date.
+
+        Output:
+        pd.DataFrame: DataFrame with yearly mean flow for defined dates.
+
+        Description: Calculates the yearly mean flow for a given date range from a DataFrame. 
+        Groups by year, computes mean flow, and returns a DataFrame.
+        """
+        strt_mnth = start_date.month,
+        end_mnth = end_date.month
+        strt_day = start_date.day
+        end_day = end_date.day
+
+        # Creating dataframe from input dates
+        if strt_mnth == end_mnth:
+            select_df = df[(df.index.month == strt_mnth) &
+                           (df.index.day >= strt_day) & 
+                           (df.index.day <= end_day)] 
+        else:
+            select_df = df[((df.index.month == strt_mnth) &
+                            (df.index.day >= strt_day)) |
+                           ((df.index.month == end_mnth) &
+                            (df.index.day <= end_day))]
+        df_mean_by_year = (
+            select_df.groupby(select_df.index.year)[['flow']].mean())
+        df_mean_by_year.index = (
+            pd.to_datetime(df_mean_by_year.index, format='%Y'))
+        df_mean_by_year.index.name = 'year'
+        return (df_mean_by_year)
+
+    # Using function to create dataframes that will be used for forecasts
+    pst2wk_mean_df = find_yearly_flow_mean(
+        start_date=pst_2wk_strt_d8, end_date=last_day_d8)
+    wk1_hist_mean_df = find_yearly_flow_mean(
+        start_date=wk1_predict_date,
+        end_date=(wk2_predict_d8 - pd.DateOffset(days=1)))
+    wk2_hist_mean_df = find_yearly_flow_mean(
+        start_date=wk2_predict_d8, 
+        end_date=(wk2_predict_d8 + pd.DateOffset(days=6)))
+
+    # Finding similar flow yrs from historical past 2 wks & trim_range input
+    crnt_yr_pst2wk_mean = pst2wk_mean_df[
+        (pst2wk_mean_df.index.year == crnt_yr)].iloc[0, 0]
+    up_bound = crnt_yr_pst2wk_mean*(1+trim_range)
+    low_bound = crnt_yr_pst2wk_mean*(1-trim_range)
+    year_list = (pst2wk_mean_df[(pst2wk_mean_df['flow'] <= up_bound) &
+                                (pst2wk_mean_df['flow'] >=
+                                 low_bound)]).index.tolist()
+
+    # Taking DFs from above function and finding a forecast w/in input StdDev
+    def find_flow_forecast_by_week(
+            week_df=wk1_hist_mean_df, year_list=year_list):
+        """
+        Finds the flow forecast for a given week.
+
+        Inputs:
+         - week_df (pd.DataFrame): DataFrame for the week.
+         - year_list (list): List of years to consider.
+
+        Output:
+            float: Flow forecast for the week.
+
+        Description:
+            Forecasts flow for a specific week based on historical data. 
+            Filters relevant years, determines a flow range, 
+                and predicts the mean flow for the week.
+         """
+        in_years_df = week_df[(week_df.index.isin(year_list))]
+        range = ((in_years_df['flow'].std())*(std_size))/2
+        upr_bnd = (in_years_df['flow'].mean()) + range
+        flow_predict = round(in_years_df[(in_years_df['flow'] <= upr_bnd)]
+                             ['flow'].mean(), 1)
+        return flow_predict
+
+    forecast_1wk = find_flow_forecast_by_week()
+    forecast_2wk = find_flow_forecast_by_week(week_df=wk2_hist_mean_df)
+
+    # Creating plots of historical & forecast for visual reference 
+    def plot_week_flow_avg_by_year():
+        """Plots Historical and forecasted weekly avg flow within the 1 week & 2 week out dates
+
+        Inputs (all automatically input from above functions):
+            - forecast_1wk
+            - wk1_predict_yr
+            - forecast_2wk
+            - wk2_predict_yr
+            - wk1_hist_mean_df
+            - wk2_hist_mean_df
+
+        Output:
+            - Plot of weekly historical avgs and forecasts, years used for forecast calcs
+
+        """
+        wk1_predict_df = pd.DataFrame({'flow': [forecast_1wk]}, 
+                                      index=[datetime(wk1_predict_yr, 1, 1)])
+        wk1_predict_df.index.name = 'year'
+        wk2_predict_df = pd.DataFrame({'flow': [forecast_2wk]}, 
+                                      index=[datetime(wk2_predict_yr, 1, 1)])
+        wk2_predict_df.index.name = 'year'
+
+        plt.style.use('seaborn-v0_8-darkgrid')
+        wk1_hist_mean_df['flow'].plot(label='Historical week of ' +
+                                      (wk1_predict_date.strftime('%m-%d')),
+                                      color='red')
+        wk1_predict_df['flow'].plot(
+            label=wk1_predict_date.strftime('%m-%d-%y'),
+            color='red', marker='o', markersize=3)
+        wk2_hist_mean_df['flow'].plot(label='Historical week of ' +
+                                      (wk2_predict_d8.strftime('%m-%d')),
+                                      color='blue')
+        wk2_predict_df['flow'].plot(label=wk2_predict_d8.strftime('%m-%d-%y'),
+                                    color='blue', marker='o', markersize=3)
+
+        # Plotting years that were used for forecast calc
+        for year in year_list:
+            plt.scatter(year, 0, color='black', marker="|", s=5)  
+        plt.xlim(datetime(1989, 1, 1), datetime(2025, 1, 1))
+        plt.legend()        
+        plt.xlabel('Year')
+        plt.ylabel('Avg Weekly Flow (CFS)')
+        plt.title('Weekly Average and Forecast Flows for the weeks of ' +
+                  (wk1_predict_date.strftime('%m-%d')) +
+                  ' & ' + (wk2_predict_d8.strftime('%m-%d')))
+    plot_week_flow_avg_by_year()
+
+    print("Forecast for week of", wk1_predict_date.date(), "(wk1):",
+          forecast_1wk, "cfs")
+    print("Forecast for week of", wk2_predict_d8.date(), "(wk2):",
+          forecast_2wk, "cfs")
+
+
+find_streamflow_prediction_from_usgs_stream(
+    wk1_predict_date=forecast_date_week1)
+
+
+# %%
